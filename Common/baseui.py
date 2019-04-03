5ab3f4b5c3225ec30cc39bda5203fc60cc026e65
薛鹏垒 <wx_7335175c637f4bc688728fb02bd91e92@git.code.tencent.com>
2019-04-03 11:30:25 +0800

    '增加iframe切换'

--- Common/baseui.py
+++ Common/baseui.py
@@ -2,11 +2,77 @@
 # -*- coding: utf-8 -*-
 import time
 
+import allure
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 
-def send_keys(driver,xpath,text):
-    username = driver.find_element_by_xpath(xpath)
-    # 清空
-    username.clear()
-    # 填值
-    username.send_keys(text)
-    time.sleep(2)
+
+
+def shot(func):
+    def function(*args, **kwargs):
+        allure.attach(args[0].driver.get_screenshot_as_png(), args[1] + '之前', allure.attachment_type.PNG)
+
+        func(*args, **kwargs)
+        allure.attach(args[0].driver.get_screenshot_as_png(), args[1] + '之后', allure.attachment_type.PNG)
+
+    return function
+
+
+class baseUI():
+
+    def __init__(self,driver):
+        self.driver = driver
+
+    def local_element(self,xpath):
+       return WebDriverWait(self.driver, 5, 0.5).until(EC.presence_of_element_located((By.XPATH,xpath)))
+
+    @shot
+    def send_keys(self,step,xpath,text):
+        '''
+        文本输入框清空并填值
+        :param step:操作步骤
+        :param xpath: xpath
+        :param text: 填的值
+        :return:
+        '''
+        element = self.local_element(xpath)
+        element.clear()
+        element.send_keys(text)
+
+
+    @shot
+    def click(self,step,xpath):
+        '''
+        #点击操作
+        :param step: 操作步骤
+        :param xpath: xpath
+        :return:
+        '''
+        element = self.local_element(xpath)
+        element.click()
+    @shot
+    def scroll_screen(self,step):
+        '''
+        #滚动窗口
+        :param step:操作步骤
+        :return:
+        '''
+        js = "var q=document.documentElement.scrollTop=10000"
+        self.driver.execute_script(js)
+    @shot
+    def switch_to_frame(self,step,xpath):
+        '''
+        #切换到iframe里边
+        :param step:操作步骤
+        :param xpath: xpath
+        :return:
+        '''
+        element = self.local_element(xpath)
+        self.driver.switch_to.frame(element)
+    def switch_to_content(self,step):
+        '''
+        #切出iframe，回到默认页面
+        :param step:操作步骤
+        :return:
+        '''
--- TestCase/testui/test_firstuidemo.py
+++ TestCase/testui/test_firstuidemo.py
@@ -5,9 +5,10 @@ from selenium import webdriver
 import time
 import os
 
-from Common.baseui import *
+
 
 from Common.Assert import Assertion
+from Common.baseui import baseUI
 
 
 class TestFirstUIDemo:
@@ -57,94 +58,48 @@ class TestFirstUIDemo:
         time.sleep(2)
 
     def test_demo2(self,driver):
+        base = baseUI(driver)
 
         #打开网址
         driver.get("http://192.168.60.132/#/login")
         #输入用户名//input[@name='username']
-        send_keys(driver,"//input[@name='username']","admin")
+        base.send_keys("输入用户名","//input[@name='username']","admin")
         #输入密码//input[@name='password']
-        send_keys(driver, "//input[@name='password']", "123456")
+        base.send_keys("输入密码","//input[@name='password']", "123456")
         #点击登录(//span[contains(text(),'登录')])[1]
-        login_button = driver.find_element_by_xpath("(//span[contains(text(),'登录')])[1]")
-        login_button.click()
-        time.sleep(2)
-        #点击商品(//span[contains(text(),'商品')])[1]
-        sp_button = driver.find_element_by_xpath("(//span[contains(text(),'商品')])[1]")
-        sp_button.click()
-        time.sleep(2)
-        #点击添加商品//span[contains(text(),'添加商品')]
-        addsp_button = driver.find_element_by_xpath("//span[contains(text(),'添加商品')]")
-        addsp_button.click()
-        time.sleep(2)
-        #点击商品分类//label[contains(text(),'商品分类：')]/following-sibling::div/span
-        spfl_button = driver.find_element_by_xpath("//label[contains(text(),'商品分类：')]/following-sibling::div/span")
-        spfl_button.click()
-        time.sleep(2)
-        #点击汽车用品//li[contains(text(),'汽车用品')]
-        driver.find_element_by_xpath("//li[contains(text(),'汽车用品')]").click()
-        time.sleep(2)
-        #点击全新整车//li[contains(text(),'全新整车')]
-        driver.find_element_by_xpath("//li[contains(text(),'全新整车')]").click()
-        time.sleep(2)
-        #输入商品名称//label[contains(text(),'商品名称：')]/following-sibling::div//input
-        sp_name = driver.find_element_by_xpath("//label[contains(text(),'商品名称：')]/following-sibling::div//input")
-        # 清空
-        sp_name.clear()
-        # 填值
-        sp_name.send_keys("凯迪拉克")
-        #输入副标题//label[contains(text(),'副标题：')]/following-sibling::div//input
-        f_title = driver.find_element_by_xpath("//label[contains(text(),'副标题：')]/following-sibling::div//input")
-        # 清空
-        f_title.clear()
-        # 填值
-        f_title.send_keys("astl")
-        #点击商品品牌//label[contains(text(),'商品品牌：')]/following-sibling::div/div
-        driver.find_element_by_xpath("//label[contains(text(),'商品品牌：')]/following-sibling::div/div").click()
-        time.sleep(2)
-        #点击小米//span[text()='小米']
-        driver.find_element_by_xpath("//span[text()='小米']").click()
-        time.sleep(2)
-        #输入商品介绍//label[contains(text(),'商品介绍')]/following-sibling::div//textarea
-        spjs = driver.find_element_by_xpath("//label[contains(text(),'商品介绍')]/following-sibling::div//textarea")
-        # 清空
-        spjs.clear()
-        # 填值
-        spjs.send_keys("sadfasdf")
-        #输入商品货号//label[contains(text(),'商品货号')]/following-sibling::div//input
-        sphh = driver.find_element_by_xpath("//label[contains(text(),'商品货号')]/following-sibling::div//input")
-        # 清空
-        sphh.clear()
-        # 填值
-        sphh.send_keys("12345677890")
-        #输入商品售价//label[contains(text(),'商品售价')]/following-sibling::div//input
-        spsj = driver.find_element_by_xpath("//label[contains(text(),'商品售价')]/following-sibling::div//input")
-        # 清空
-        spsj.clear()
-        # 填值
-        spsj.send_keys("9.9")
-        #输入市场价//label[contains(text(),'市场价')]/following-sibling::div//input
-        scj = driver.find_element_by_xpath("//label[contains(text(),'市场价')]/following-sibling::div//input")
-        # 清空
-        scj.clear()
-        # 填值
-        scj.send_keys("99")
-        #输入商品库存
-        #输入计量单位
-        #下拉窗口
-        js = "var q=document.documentElement.scrollTop=10000"
-        driver.execute_script(js)
-        time.sleep(2)
-        #输入商品重量
-        #输入排序
-        #点击下一步//span[text()='下一步，填写商品促销']
-        driver.find_element_by_xpath("//span[text()='下一步，填写商品促销']").click()
-        #allure.attach(driver.get_screenshot_as_png(),'测试一下',allure.attachment_type.PNG)
-        time.sleep(2)
-        #下一步，填写商品属性
-        driver.find_element_by_xpath("//span[text()='下一步，填写商品属性']").click()
+        base.click('点击登录',"(//span[contains(text(),'登录')])[1]")
+        base.click('点击商品',"//span[text()='商品']")
+        #点击添加商品
+        base.click("点击添加商品","//span[text()='添加商品']")
+        #点击商品分类
+        base.click("点击商品分类","//label[contains(text(),'商品分类')]/following-sibling::div/span")
+        #点击服装
+        base.click("点击服装","//li[contains(text(),'服装')]")
+        #点击外套
+        base.click("点击外套","//li[contains(text(),'外套')]")
+        #填写商品名称
+        base.send_keys("填写商品名称","//label[contains(text(),'商品名称')]/following-sibling::div//input","缺了个口子")
+        #填写副标题
+        base.send_keys("填写副标题","//label[contains(text(),'副标题')]/following-sibling::div//input","谁啃得")
+        #点击商品品牌
+        base.click("点击商品品牌","//label[contains(text(),'商品品牌')]/following-sibling::div/div")
+        #点击苹果
+        base.click("点击苹果","//span[text()='苹果']")
+        #滚动窗口
+        base.scroll_screen("滚动窗口")
+        #点击下一步
+        base.click("击下一步","//span[text()='下一步，填写商品促销']")
+        #点击下一步
+        base.click("点击下一步","//span[text()='下一步，填写商品属性']")
+        #上传商品图片
+        #base.send_keys("上传商品图片","//input[@name='file']/parent::*","‪D:\\1.png")
+        #切换iframe
+        base.switch_to_frame("切换到iframe","(//iframe[contains(@id,'vue-tinymce-')])[1]")
+        #填写规格参数
+        base.send_keys("填写规格参数","//body[@id='tinymce']","测试一下")
+        driver.switch_to.default_content()
+        #点击下一步
+        base.click("点击下一步","//span[contains(text(),'下一步，选择商品关联')]")
         time.sleep(2)
 
-        driver.switch_to.frame(driver.find_element_by_xpath("(//iframe[contains(@id,'vue-tinymce-')])[1]"))
-        time.sleep(2)
-        driver.find_element_by_id("tinymce").send_keys('ceshiyixia')
-        time.sleep(2)
+
