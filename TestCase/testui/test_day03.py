ccb49bbdaf3d633844f43a2e860b3561ad6d5b20
薛鹏垒 <xuepl@guoyasoft.com>
2019-04-30 15:58:35 +0800

    第三天

--- TestCase/testui/test_day03.py
+++ TestCase/testui/test_day03.py
@@ -3,22 +3,22 @@
 import os
 import time
 
+import pytest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from Common.baseui import baseUI
+from Common.read_excel import *
+
 
 
 class TestMall():
 
-
-    def test_login(self):
-        #打开浏览器
-        # 确定chromedriver.exe的位置
-        driver_path = os.path.join(os.path.dirname(__file__), "../../chromedriver/chromedriver.exe")
-        # 打开浏览器
-        driver = webdriver.Chrome(driver_path)
-        driver.maximize_window()  # 最大化浏览器
-        driver.set_page_load_timeout(10)  # 网页加载超时为10s
-        driver.set_script_timeout(10)  # js脚本运行超时10s
-        driver.implicitly_wait(10)  # 元素查找超时时间10s
+    @pytest.mark.s
+    def test_login(self,driver):
+        base = baseUI(driver)
         #打开网址
         driver.get("http://192.168.1.137/#/login")
         #定位并处理异常弹框//span[contains(text(),'残忍拒绝')]
@@ -35,17 +35,35 @@ class TestMall():
         #password.clear()
         #password.send_keys("123456")
         #定位登录按钮(//span[contains(text(),'登录')])[1]
-        driver.find_element_by_xpath("(//span[contains(text(),'登录')])[1]").click()
+        base.click("点击登录按钮","(//span[contains(text(),'登录')])[1]")
         #点击登录按钮
+
         try:
-            driver.find_element_by_xpath("//span[contains(text(),'残忍拒绝')]").click()
+            base.click("点击残忍拒绝", "//span[contains(text(),'残忍拒绝')]")
         except:
             pass
+        # 线程等待，特定程序执行到该语句就会休眠给定的时间，时间到，继续执行下边的代码。写代码调试的时候用
         time.sleep(2)
-        driver.find_element_by_xpath("(//span[contains(text(),'登录')])[1]").click()
+        #selenium提供了一个显示等待，特点：判断页面元素是否达到某种状态，如果条件满足程序就会往下执行，如果不满足，超时时间达到，就会抛出异常，代码会终止
+        #WebDriverWait(driver, 5, 0.3).until(EC.presence_of_element_located((By.XPATH, "(//span[contains(text(),'登录')])[1]"))).click()
+        base.click("点击登录按钮", "(//span[contains(text(),'登录')])[1]")
         time.sleep(1)
         #断言页面是否跳转到首页
-        print(driver.page_source)
         assert "首页" in driver.page_source
+
+    def test_order_1(self,driver):
+
+        print("查询订单")
+
         pass
 
+    l_name = read_excel_list("d:\\data.xlsx")
+
+    @pytest.fixture(params=l_name)
+    def name(self,request):
+        return request.param
+
+    def test_order_2(self,name):
+        print(name[0])
+
+        pass
